f87fedf912fa05133ce6045acc2e885a788a6143
Heather Hern <heathernewlin1@gmail.com>
2020-11-02 14:55:19 -0500

    Update schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -1,5 +1,4 @@
-### Schema
-
+DROP TABLE IF EXISTS burger_db;
 CREATE DATABASE burger_db;
 USE burger_db;
 
@@ -7,6 +6,6 @@ CREATE TABLE burgers
 (
 	id int NOT NULL AUTO_INCREMENT,
 	name varchar(255) NOT NULL,
-	show BOOLEAN DEFAULT true,
+	showing BOOLEAN DEFAULT true,
 	PRIMARY KEY (id)
 );
